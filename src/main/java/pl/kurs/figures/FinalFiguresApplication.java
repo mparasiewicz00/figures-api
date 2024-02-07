67517c386f8b1171bd4f1f974c5c6ac1cab959e6
mateuszparasiewicz <m.parasiewicz00@gmail.com>
2024-02-07 12:01:22 +0100

    Initial commit

---
+++ src/main/java/pl/kurs/figures/FinalFiguresApplication.java
@@ -0,0 +1,13 @@
+package pl.kurs.figures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalFiguresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalFiguresApplication.class, args);
+    }
+
+}
