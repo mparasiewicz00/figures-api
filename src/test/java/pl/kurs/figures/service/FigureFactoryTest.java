fc7442be5dc866e1677c57067768e92be3f14f32
Mateusz Parasiewicz <80644390+mparasiewicz00@users.noreply.github.com>
2024-02-20 18:01:23 +0100

    Adding tests for figureFactory (#6)

--- src/test/java/pl/kurs/figures/service/FigureFactoryTest.java
+++ src/test/java/pl/kurs/figures/service/FigureFactoryTest.java
@@ -1,5 +1,44 @@
 package pl.kurs.figures.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.kurs.figures.command.CreateFigureCommand;
+import pl.kurs.figures.exceptions.ShapeNotExistException;
+import pl.kurs.figures.model.Circle;
+import pl.kurs.figures.model.Figure;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(MockitoExtension.class)
 class FigureFactoryTest {
+    FigureFactory figureFactory = new FigureFactory();
+
+    @Test
+    void shouldCreateFigureWhenCorrectParametersPassed() {
+        //given
+        CreateFigureCommand command = new CreateFigureCommand("CIRCLE", List.of(20.0));
+
+        //when
+        Figure figure = figureFactory.createFigure(command.getType(), command.getParameters());
+
+        //then
+        assertAll(
+                () -> assertEquals("CIRCLE", figure.getType()),
+                () -> assertInstanceOf(Circle.class, figure)
+        );
+    }
+
+
+    @Test
+    void shouldThrowExceptionWhenIncorrectTypePassed() {
+        CreateFigureCommand command = new CreateFigureCommand("TRIANGLE", List.of(10.0));
+
+        Exception exception = assertThrows(ShapeNotExistException.class, () -> figureFactory.createFigure(command.getType(), command.getParameters()));
+        assertTrue(exception.getMessage().contains("Wrong shape passed"));
+    }
 
 }
